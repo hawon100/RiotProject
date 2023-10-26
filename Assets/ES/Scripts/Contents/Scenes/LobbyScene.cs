9c9b7566ca3aafa09fd80691d5be607ed18a7fa4
hawon100 <hawonoj002@gmail.com>
2023-10-26 09:08:32 +0900

    Fixed
    fixed

--- Assets/ES/Scripts/Contents/Scenes/LobbyScene.cs
+++ Assets/ES/Scripts/Contents/Scenes/LobbyScene.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class LobbyScene : BaseScene
 {
+    public int MapSpawnIndex = 0;
+
     protected override void Init()
     {
         base.Init();
 
         SceneType = Define.Scene.Lobby;
+
+        List<GameObject> mapObj = new();
+        for(int i = 0; i < MapSpawnIndex; i++)
+        {
+            mapObj.Add(Managers.Resource.Instantiate("Stage/Map"));
+        }
     }
 
     private void Update()
