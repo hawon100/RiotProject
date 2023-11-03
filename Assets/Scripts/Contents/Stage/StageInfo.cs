918b2b9ddbf78a3577e4c180af65fe604bcda61a
hawon100 <hawonoj002@gmail.com>
2023-11-03 13:09:36 +0900

    StageUI

--- Assets/Scripts/Contents/Stage/StageInfo.cs
+++ Assets/Scripts/Contents/Stage/StageInfo.cs
@@ -11,9 +11,20 @@ public class StageInfo : MonoBehaviour
 
     private void Update()
     {
-        var stageIndex = RoundData.Instance.stageIndex;
-        var mapIndex = RoundData.Instance.mapIndex;
+        var stageIndex = RoundData.Instance.stageIndex + 1;
+        var mapIndex = RoundData.Instance.mapIndex + 1;
 
+        stageText.text = stageIndex.ToString() + " - " + mapIndex.ToString();
 
+        if (stageIndex == 1)
+        {
+            switch (mapIndex)
+            {
+                case 1: player.position = path[1].position; break;
+                case 2: player.position = path[2].position; break;
+                case 3: player.position = path[3].position; break;
+                case 4: player.position = path[4].position; break;
+            }
+        }
     }
 }
