1f2f1ea15fbe3c8c3b3f7ad744bc0887d6be118d
Smin1004 <sminnims1004@naver.com>
2023-11-14 18:34:48 +0900

    dataRemove

--- Assets/Scripts/Managers/Contents/Map/MeshCreator.cs
+++ Assets/Scripts/Managers/Contents/Map/MeshCreator.cs
@@ -11,56 +11,79 @@ public class MeshCreator : MonoBehaviour
 
     void CreateMesh()
     {
-        // Vector3[] vertices = { 
-        //     new(-0.5f, 0.5f, 0.5f), 
-        //     new(0.5f, 0.5f, 0.5f), 
-        //     new(0.5f, -0.5f, 0.5f), 
-        //     new(-0.5f, -0.5f, 0.5f),
-        //     new(-0.5f, 0.5f, -0.5f), 
-        //     new(0.5f, 0.5f, -0.5f), 
-        //     new(0.5f, -0.5f, -0.5f), 
-        //     new(-0.5f, -0.5f, -0.5f)
-        // };
-
         Vector3[] vertices = {
-			new Vector3 (0, 0, 0),
-			new Vector3 (1, 0, 0),
-			new Vector3 (1, 1, 0),
-			new Vector3 (0, 1, 0),
-			new Vector3 (0, 1, 1),
-			new Vector3 (1, 1, 1),
-			new Vector3 (1, 0, 1),
-			new Vector3 (0, 0, 1),
+			
 		};
 
-        int[] triangles = new int[]
-        {
-            // 0, 2, 1, //face front
-			// 0, 3, 2,
+		int[] triangles = {
+			0, 2, 1, //face front
+			0, 3, 2,
 
 			2, 3, 4, //face top
 			2, 4, 5,
 
-			// 1, 2, 5, //face right
-			// 1, 5, 6,
+			1, 2, 5, //face right
+			1, 5, 6,
 
-			// 0, 7, 4, //face left
-			// 0, 4, 3,
+			0, 7, 4, //face left
+			0, 4, 3,
 
 			5, 4, 7, //face back
 			5, 7, 6,
-            
-			// 0, 6, 7, //face bottom
-			// 0, 1, 6
-        };
+
+			0, 6, 7, //face bottom
+			0, 1, 6
+		};
+
+        Vector2 [] uvs = new Vector2 [vertices.Length]; 
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+        }
 
         Mesh mesh = new();
 
         GetComponent<MeshFilter>().mesh = mesh;
         mesh.Clear();
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        mesh.triangles = triangles; 
+        mesh.uv = uvs;
         mesh.Optimize();
         mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        mesh.RecalculateBounds();
+    }
+
+    private Vector3[] GetVertices(){
+        float cubeLength = transform.localScale.x;
+        float cubeWidth = transform.localScale.y;
+        float cubeHeight = transform.localScale.z;
+
+		Vector3 v0 = new(cubeLength * -0.5f, cubeWidth * -0.5f, cubeHeight * 0.5f);
+		Vector3 v1 = new(cubeLength * 0.5f, cubeWidth * -0.5f, cubeHeight * 0.5f);
+        Vector3 v2 = new(cubeLength * 0.5f, cubeWidth * -0.5f, cubeHeight * -0.5f);
+        Vector3 v3 = new(cubeLength * -0.5f, cubeWidth * -0.5f, cubeHeight * -0.5f);
+		Vector3 v4 = new(cubeLength * -0.5f, cubeWidth * 0.5f, cubeHeight * 0.5f);
+		Vector3 v5 = new(cubeLength * 0.5f, cubeWidth * 0.5f, cubeHeight * 0.5f);
+		Vector3 v6 = new(cubeLength * 0.5f, cubeWidth * 0.5f, cubeHeight * -0.5f);
+		Vector3 v7 = new(cubeLength * -0.5f, cubeWidth * 0.5f, cubeHeight * -0.5f);
+
+        Vector3[] vertices = new Vector3[]
+        {
+            v0, v1, v2, v3, //bottom
+
+            v7, v4, v0, v3, //left
+
+            v4, v5, v1, v0, //front
+
+            v6, v7, v3, v2, //back
+
+            v5, v6, v2, v1, //right
+
+            v7, v6, v5, v4  //top
+        };
+
+        return vertices;
     }
 }
---
+++ Assets/Scripts/Managers/Contents/Map/simple-cube-mesh-generation-unity3d-master/MeshGenerationDemo.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshGenerationDemo : MonoBehaviour
+{
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+
+    private float cubeLength;
+    private float cubeWidth;
+    private float cubeHeight;
+
+    private Mesh cubeMesh;
+
+    void Start()
+    {
+        MakeCube();
+    }
+
+    private Vector3[] GetVertices()
+    {
+        Vector3 vertice_0 = new Vector3(-cubeLength * .5f, -cubeWidth * .5f, cubeHeight * .5f);
+        Vector3 vertice_1 = new Vector3(cubeLength * .5f, -cubeWidth * .5f, cubeHeight * .5f);
+        Vector3 vertice_2 = new Vector3(cubeLength * .5f, -cubeWidth * .5f, -cubeHeight * .5f);
+        Vector3 vertice_3 = new Vector3(-cubeLength * .5f, -cubeWidth * .5f, -cubeHeight * .5f);
+
+        Vector3 vertice_4 = new Vector3(-cubeLength * .5f, cubeWidth * .5f, cubeHeight * .5f);
+        Vector3 vertice_5 = new Vector3(cubeLength * .5f, cubeWidth * .5f, cubeHeight * .5f);
+        Vector3 vertice_6 = new Vector3(cubeLength * .5f, cubeWidth * .5f, -cubeHeight * .5f);
+        Vector3 vertice_7 = new Vector3(-cubeLength * .5f, cubeWidth * .5f, -cubeHeight * .5f);
+
+        Vector3[] vertices = new Vector3[]
+        {					
+			vertice_7, vertice_4, vertice_0, vertice_3, // Left Polygon
+					
+			vertice_4, vertice_5, vertice_1, vertice_0, // Front Polygon
+					
+			vertice_6, vertice_7, vertice_3, vertice_2, // Back Polygon
+					
+			vertice_5, vertice_6, vertice_2, vertice_1, // Right Polygon
+				
+			vertice_7, vertice_6, vertice_5, vertice_4 // Top Polygon
+		};
+
+        return vertices;
+    }
+
+    private Vector3[] GetNormals()
+    {
+        Vector3 up = Vector3.up;
+        Vector3 front = Vector3.forward;
+        Vector3 back = Vector3.back;
+        Vector3 left = Vector3.left;
+        Vector3 right = Vector3.right;
+
+        Vector3[] normales = new Vector3[]
+        {					
+			left, left, left, left, // LEFT Side Render
+					
+			front, front, front, front, // FRONT Side Render
+									
+			back, back, back, back, // BACK Side Render
+					
+			right, right, right, right, // RIGTH Side Render
+
+			up, up, up, up // UP Side Render
+		};
+
+        return normales;
+    }
+
+    private Vector2[] GetUVsMap()
+    {
+        Vector2 _00_CORDINATES = new Vector2(0f, 0f);
+        Vector2 _10_CORDINATES = new Vector2(1f, 0f);
+        Vector2 _01_CORDINATES = new Vector2(0f, 1f);
+        Vector2 _11_CORDINATES = new Vector2(1f, 1f);
+
+        Vector2[] uvs = new Vector2[]
+        {					
+			_11_CORDINATES, _01_CORDINATES, _00_CORDINATES, _10_CORDINATES, // Left
+				
+			_11_CORDINATES, _01_CORDINATES, _00_CORDINATES, _10_CORDINATES, // Front					
+				
+			_11_CORDINATES, _01_CORDINATES, _00_CORDINATES, _10_CORDINATES, // Back
+				
+			_11_CORDINATES, _01_CORDINATES, _00_CORDINATES, _10_CORDINATES, // Right					
+				
+			_11_CORDINATES, _01_CORDINATES, _00_CORDINATES, _10_CORDINATES, // Top
+        };
+        return uvs;
+    }
+
+    private int[] GetTriangles()
+    {
+        int[] triangles = new int[]
+        {
+		// Cube Left Side Triangles
+			3, 1, 0,
+            3, 2, 1,			
+					
+		// Cube Front Side Triangles
+			3 + 4 * 1, 1 + 4 * 1, 0 + 4 * 1,
+            3 + 4 * 1, 2 + 4 * 1, 1 + 4 * 1,
+					
+		// Cube Back Side Triangles
+			3 + 4 * 2, 1 + 4 * 2, 0 + 4 * 2,
+            3 + 4 * 2, 2 + 4 * 2, 1 + 4 * 2,
+					
+		// Cube Rigth Side Triangles
+			3 + 4 * 3, 1 + 4 * 3, 0 + 4 * 3,
+            3 + 4 * 3, 2 + 4 * 3, 1 + 4 * 3,
+					
+		// Cube Top Side Triangles
+			3 + 4 * 4, 1 + 4 * 4, 0 + 4 * 4,
+            3 + 4 * 4, 2 + 4 * 4, 1 + 4 * 4,		
+        };
+
+        return triangles;
+    }
+
+    private Mesh GetCubeMesh()
+    {
+
+        if (GetComponent<MeshFilter>() == null)
+        {
+            Mesh mesh;
+            MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+            mesh = filter.mesh;
+            mesh.Clear();
+            return mesh;
+        }
+        else
+        {
+            return gameObject.GetComponent<MeshFilter>().mesh;
+        }
+    }
+
+    void MakeCube()
+    {
+        cubeMesh = GetCubeMesh();
+        cubeMesh.vertices = GetVertices();
+        cubeMesh.normals = GetNormals();
+        cubeMesh.uv = GetUVsMap();
+        cubeMesh.triangles = GetTriangles();
+        cubeMesh.RecalculateBounds();
+        cubeMesh.RecalculateNormals();
+        cubeMesh.Optimize();
+    }
+
+
+}
