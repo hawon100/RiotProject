905470c58eff9c459869d65fe7f11a1f882cc532
hawon100 <hawonoj002@gmail.com>
2023-11-14 20:59:12 +0900

    ParticleAdd

---
+++ Assets/Scripts/Contents/UI/Ingame/DamageUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageUI : MonoBehaviour
+{
+    public static DamageUI Instance { get; private set; }
+    [SerializeField] private Image damage_ui;
+    [SerializeField] private float startAlpha;
+    [SerializeField] private float alphaSpeed;
+    private bool isDamage;
+    private float curAlpha;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (isDamage)
+        {
+            damage_ui.color = new Color(1, 1, 1, curAlpha);
+            curAlpha-=Time.deltaTime;
+            if(curAlpha <= 0)
+                isDamage = false;
+        }
+    }
+
+    public void Damage()
+    {
+        isDamage = true;
+        curAlpha = startAlpha;
+        //StartCoroutine(alpha(damage_ui, 1));
+    }
+
+    //IEnumerator alpha(Image image, int sec)
+    //{
+    //    isDamage = true;
+    //    float timer = 0f;
+    //    while (timer <= sec)
+    //    {
+    //        if(!isDamage) yield break;
+    //        image.color = new Color(1, 1, 1, 1 - timer / sec);
+    //        timer += Time.deltaTime;
+    //        yield return null;
+    //    }
+    //    isDamage = false;
+    //}
+}
