6a39ac9c1e5d5955c0daef7ed8171c8d0b00c5a0
hawon100 <hawonoj002@gmail.com>
2023-11-30 14:42:16 +0900

    RankingManager

---
+++ Assets/Scripts/Managers/Contents/RankingManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingManager : MonoBehaviour
+{
+    private int[] bestScore = new int[5];
+
+    public void ScoreSet(int currentScore) //랭킹
+    {
+        Managers.Data.LoadData<RankData>("RankData").Rank[0].bestScore = currentScore;
+        
+        var tmpScore = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            bestScore[i] = Managers.Data.LoadData<RankData>("RankData").Rank[i].bestScore;
+            
+            while(bestScore[i] < currentScore)
+            {
+                tmpScore = bestScore[i];
+                bestScore[i] = currentScore;
+
+                Managers.Data.LoadData<RankData>("RankData").Rank[i].bestScore = bestScore[i];
+
+                currentScore = tmpScore;
+            }
+        }
+
+        for(int i = 0; i < 5; i++)
+        {
+            Managers.Data.LoadData<RankData>("RankData").Rank[i].bestScore = bestScore[i];
+        }
+    }
+}
